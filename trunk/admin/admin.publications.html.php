2d654b78c326207e4ac37a3388dae21ac0fd2688
Dennis84225 <Dennis84225@6062a6f1-c74d-0410-91ca-61cb777b4f4f>
2008-05-27 14:26:16 +0000

    Fix. Some minor fixes on XML and HTML formatting.

--- trunk/admin/admin.publications.html.php
+++ trunk/admin/admin.publications.html.php
@@ -7,19 +7,19 @@ class HTML_publications {
     $editor =& JFactory::getEditor();
     ?>
     <form action="index.php" method="post" 
-       name="adminForm" id="adminForm"> 
+        name="adminForm" id="adminForm"> 
       <fieldset class="adminform"> 
         <legend>Details</legend>
         <table class="admintable">
           <tr> 
-	    <td width="100" align="right" class="key"> 
-	      Title:</td> 
-	    <td> 
+	        <td width="100" align="right" class="key"> 
+	          Title:</td> 
+	        <td> 
               <input class="text_area" type="text" name="title" 
-	          id="name" size="50" maxlength="250" 
-		  value="<?php echo $row->title;?>" /> 
-	    </td>
-	  </tr>
+	              id="name" size="50" maxlength="250" 
+		          value="<?php echo $row->title;?>" /> 
+	        </td>
+	      </tr>
         </table>
       </fieldset> 
       <input type="hidden" name="id" value="<?php echo $row->id; ?>" /> 
@@ -33,45 +33,46 @@ class HTML_publications {
     ?>
     <form action="index.php" method="post" name="adminForm"> 
       <table class="adminlist"> 
-	<thead> 
-	  <tr> 
-	    <th width="20">
+	      <thead> 
+	        <tr> 
+	          <th width="20">
               <input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count( $rows ); ?>);" />
             </th>
-            <th class="title">Title</th> 
+            <th class="title">Title</th>
+          </tr> 
         </thead>
         <?php
         jimport('joomla.filter.output');
         $k = 0;
         for ($i=0, $n=count( $rows ); $i < $n; $i++) {
-	  $row = &$rows[$i]; 
-	  $checked = JHTML::_('grid.id', $i, $row->id );
-	  //$published = JHTML::_('grid.published', $row, $i );
-	  $link = JFilterOutput::ampReplace( 'index.php?option=' . $option . '&task=edit&cid[]='. $row->id );
-	  ?>
+	        $row = &$rows[$i]; 
+	        $checked = JHTML::_('grid.id', $i, $row->id );
+	        //$published = JHTML::_('grid.published', $row, $i );
+	        $link = JFilterOutput::ampReplace( 'index.php?option=' . $option . '&task=edit&cid[]='. $row->id );
+	        ?>
           <tr class="<?php echo "row$k"; ?>">
             <td> 
-	      <?php echo $checked; ?> 
-	    </td>
+	            <?php echo $checked; ?> 
+	          </td>
             <td>
-	      <a href="<?php echo $link; ?>"> 
+	            <a href="<?php echo $link; ?>"> 
                 <?php echo $row->title; ?>
               </a>
-	    </td>
+	          </td>
           </tr> 
-	  <?php 
+	        <?php 
             $k = 1 - $k; 
-	} 
-	?>
+	      }
+	      ?>
         <tfoot> 
-	  <td colspan="7"><?php echo $pageNav->getListFooter();?></td> 
-	</tfoot>
-        </table>
-	<input type="hidden" name="option" value="<?php echo $option;?>" /> 
-	<input type="hidden" name="task" value="" /> 
-	<input type="hidden" name="boxchecked" value="0" /> 
-      </form> 
-      <?php
+	        <td colspan="7"><?php echo $pageNav->getListFooter();?></td> 
+	      </tfoot>
+      </table>
+      <input type="hidden" name="option" value="<?php echo $option;?>" /> 
+	    <input type="hidden" name="task" value="" /> 
+	    <input type="hidden" name="boxchecked" value="0" /> 
+    </form> 
+    <?php
   } 
 }
 
