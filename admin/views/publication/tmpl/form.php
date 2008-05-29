80f738a133ae8107e1dc884f731ba76bbc013c3a
dennis84225 <dennis84225@6062a6f1-c74d-0410-91ca-61cb777b4f4f>
2008-05-29 19:19:47 +0000

    Enhancement. Added several essential input fields in the admin form so that they can be shown on the table.

--- admin/views/publication/tmpl/form.php
+++ admin/views/publication/tmpl/form.php
@@ -3,19 +3,63 @@
   <fieldset class="adminform">
     <legend>Details</legend>
     <table class="admintable">
+      <!-- Title  -->
       <tr>
         <td width="100" align="right" class="key">
-          Title:
+          <label for="title">
+            Title:
+          </label>
         </td>
         <td>
           <input class="text_area" type="text" name="title"
-              id="name" size="50" maxlength="250"
+              id="title" size="50" maxlength="250"
               value="<?php echo $this->row->title;?>" />
+        </td>
+      </tr>
+      <!-- Entry Type  -->
+      <tr>
+        <td width="100" align="right" class="key">
+          <label for="entrytype">
+            Entry Type:
+          </label>
+        </td>
+        <td>
+          <?php 
+            echo $this->lists['entrytype'];
+          ?>
+        </td>
+      </tr>
+      <!-- Address  -->
+      <tr>
+        <td width="100" align="right" class="key">
+          <label for="address">
+            Address:
+          </label>
+        </td>
+        <td>
+          <input class="text_area" type="text" name="address"
+              id="address" size="100" maxlength="250"
+              value="<?php echo $this->row->address;?>" />
+        </td>
+      </tr>
+      <!-- Annote TODO  -->
+      <!-- Author  -->
+      <tr>
+        <td width="100" align="right" class="key">
+          <label for="author">
+            Author(s):
+          </label>
+        </td>
+        <td>
+          <input class="text_area" type="text" name="author"
+              id="author" size="100" maxlength="250"
+              value="<?php echo $this->row->author;?>" />
         </td>
       </tr>
     </table>
   </fieldset>
   <input type="hidden" name="id" value="<?php echo $this->row->id; ?>" />
+  <input type="hidden" name="published" value="<?php echo $this->row->published; ?>" />
   <input type="hidden" name="option" value="<?php echo $option;?>" />
   <input type="hidden" name="task" value="" />
 </form>
--- admin/views/publication/view.html.php
+++ admin/views/publication/view.html.php
@@ -17,8 +17,32 @@ class PublicationsViewPublication extends JView {
 
   function _displayForm($tpl) {
     $pub =& $this->get('Data');
+    $lists = array();
+    
+    $lists['entrytype'] = JHTML::_('select.genericlist', $this->_createEntryTypeInput(), 
+        'entrytype', 'class="inputbox" '. '', 'value', 'text', $pub->entrytype );
+    $this->assignRef('lists',$lists);
     $this->assignRef('row', $pub);
     parent::display($tpl);
+  }
+
+  function _createEntryTypeInput() {
+    $entrytype = array ( 
+      '0' => array('value' => 'article', 'text' => 'Article'), 
+      '1' => array('value' => 'booklet', 'text' => 'Booklet'), 
+      '2' => array('value' => 'conference', 'text' => 'Conference'), 
+      '3' => array('value' => 'inbook', 'text' => 'Inbook'),
+      '4' => array('value' => 'incollection', 'text' => 'Incollection'),
+      '5' => array('value' => 'inproceedings', 'text' => 'Inproceedings'),
+      '6' => array('value' => 'manual', 'text' => 'Manual'),
+      '7' => array('value' => 'mastersthesis', 'text' => 'Mastersthesis'),
+      '8' => array('value' => 'misc', 'text' => 'Misc'),
+      '9' => array('value' => 'phdthesis', 'text' => 'Phdthesis'),
+      '10' => array('value' => 'proceedings', 'text' => 'Proceedings'),
+      '11' => array('value' => 'techreport', 'text' => 'TechReport'),
+      '12' => array('value' => 'unpublished', 'text' => 'Unpublished')       
+    );
+    return $entrytype;
   }
 
 }
