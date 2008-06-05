288a2d0dec4fac867611e18744f11c4bf27e2884
dennis84225 <dennis84225@6062a6f1-c74d-0410-91ca-61cb777b4f4f>
2008-06-05 00:31:00 +0000

    Fix (not really). Left out a newly added file in the last submission.

---
+++ admin/views/publication/tmpl/default.php
@@ -0,0 +1,60 @@
+<?php
+defined( '_JEXEC' ) or die( 'Restricted access' );
+?>
+<table width="100%" cellpadding="0" cellspacing="0" border="0" class="contentpaneopen">
+  <tr>
+    <td width="100%" class="contentheading">
+      [Title] : <?php echo $this->publication->title; ?>
+    </td>
+  </tr>
+  <tr>
+    <table>
+      <?php          
+      foreach($this->publication as $key => $value) {
+        if($value) {
+          //special situations.
+          switch($key) {
+          case 'bibyear':
+            if(intval($value) != 0000) {
+              printRow($key, $value);
+            }
+            break;
+          case 'submitted_time':
+            if($value != '0000-00-00 00:00:00')
+              printRow($key, JHTML::_('date',$value, '%Y-%m-%d %H:%M:%S, %Z'), 'Submitted Time');
+            break;
+          case 'submitted_by':
+            printRow($key, $this->lists['submitted_by'], 'Submitted By');
+            break;
+          default:
+            printRow($key, $value);
+          }
+        }
+      }
+      ?>
+    </table>
+  </tr>
+</table>
+
+<?php
+function printRow($key, $value, $keyLabel="")
+{
+  //deal with 'bib' prefix
+  if(substr($key, 0, 3) == 'bib') {
+    $key = substr($key, 3);
+  }
+
+  if(empty($keyLabel)) {
+    $keyLabel = ucfirst($key);
+  }
+
+?>
+  <tr>
+    <td><strong><?php echo $keyLabel; ?>: </strong></td>
+    <td>
+      <?php echo $value; ?>    
+    </td>
+  </tr>
+<?php   
+}
+?>
