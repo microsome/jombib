4db85566c56c877c958427ad1d9787fd35c7ebbf
dennis84225 <dennis84225@6062a6f1-c74d-0410-91ca-61cb777b4f4f>
2008-06-19 20:09:10 +0000

    Fix. This has to be added for the previous commit to work. The component registering script was needed from the beginning but hasn't been added to the repo. config.xml and params in #__components define the author string format used in this component.

---
+++ trunk/admin/register_component.mysql.sql
@@ -0,0 +1,2 @@
+INSERT INTO `cac_components` (name, link, admin_menu_link, admin_menu_alt, `option`, admin_menu_img, params)
+       VALUES ('Publications', 'option=com_publications', 'option=com_publications', 'Manage Publications', 'com_publications', 'js/ThemeOffice/component.png', 'author_display_format=\n');
